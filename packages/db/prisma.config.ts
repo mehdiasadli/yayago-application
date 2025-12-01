69b158e7cf191826258fa6889ed80d1ec0225b27
Mehdi Asadli <mehdiasadli@azericard.com>
2025-12-01 13:51:09 +0400

    chore: update configuration for Prisma and Turbo
    - Add global environment variables to turbo.json for better configuration management.
    - Refactor dotenv configuration in prisma.config.ts to improve path handling and quiet mode.

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,17 +1,18 @@
-import path from "node:path";
-import { defineConfig, env } from "prisma/config";
-import dotenv from "dotenv";
+import path from 'node:path';
+import { defineConfig, env } from 'prisma/config';
+import dotenv from 'dotenv';
 
 dotenv.config({
-	path: "../../apps/server/.env",
+  quiet: true,
+  path: path.join(process.cwd(), '../../apps/server/.env'),
 });
 
 export default defineConfig({
-	schema: path.join("prisma", "schema"),
-	migrations: {
-		path: path.join("prisma", "migrations"),
-	},
-	datasource: {
-		url: env("DATABASE_URL"),
-	},
+  schema: path.join('prisma', 'schema'),
+  migrations: {
+    path: path.join('prisma', 'migrations'),
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
 });
